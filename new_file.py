bd0ce072c2f9bcc0f299c943ce58adfebbca51ac
Rajan <rajanlamichhane1998@gmail.com>
2024-05-25 21:48:59 +0545

    Matrix problem solved

--- new_file.py
+++ new_file.py
@@ -1,4 +1,23 @@
-# This is a new file and I will add this in the repository.
+def Solution(matrix: list[list[int]]) -> bool:
+    n = len(matrix)
+    row_list = []
+    rule = [x for x in range(1, n + 1)]
+    for row in matrix:
+        for i in range(len(row)):
+            row_list.append(row[i])
 
-# I have committed but now I changed it again.
+        if set(row_list) != set(rule):
+            return False
+        row_list.clear()
 
+    col = [[] for _ in range(len(matrix[0]))]
+
+    print(col)
+    for i in range(n):
+        for j in range(n):
+            col[j].append(matrix[i][j])
+
+    print(col)
+    return all(set(column) == set(rule) for column in col)
+
+print(Solution([[1,2,3],[2,3,1],[3,1,2]]))
