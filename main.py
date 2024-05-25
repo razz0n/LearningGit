ae130806087af0e4fc0f4d4263e891218b00b36c
Rajan <rajanlamichhane1998@gmail.com>
2024-05-25 22:09:15 +0545

    Added user input module

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+from new_file import solution
 
+def get_matrix_solution() -> list[list[int]]:
+    print("Checks whether the rows and columns in the matrix have all the number in the range n")
+    rows: int = int(input("Enter the number of rows in the matrix"))
+    columns: int = int(input("Enter the number of columns in the matrix"))
+    matrix = get_user_input(rows,columns)
+    return matrix
 
-def print_hello():
-    print("Hello, Rajan !")
-
+def get_user_input(rows: int,columns: int) -> list[list[int]]:
+    matrix = [[] for i in range(rows)
+    for i in range(len(rows)):
+        for j in range(len(columns)):
+            element: int = int(input("Enter the {}{}th element in the matrix".format(i+1,j+1)))
+            matrix[i].append(element)
+    return matrix
 
 if __name__ == '__main__':
-    print_hello()
-
-#I have changed the file now
-
-
-
-# After I moved the head to this commit and given it a little change
-
+    start_matrix = get_matrix_solution()
+    if solution(start_matrix):
+        print("The matrix is valid")
+    else:
+        print("The matrix is not valid")
